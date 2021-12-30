7c8394986594768edb49f0b774b8e0240e1a5774
dev05-aiatic <Anyigarcia07@gmail.com>
2021-12-29 22:54:12 -0500

    Nuevo

--- 01-hola_mundo/app.js
+++ 01-hola_mundo/app.js
@@ -1,3 +1,14 @@
 
 
+let nombre= "Juan";
+console.log(nombre)
+
+
+const prueba = () => {
+    let nombre= "Eva";
+    console.log(nombre)
+}
+
+prueba()
+console.log(nombre)
 console.log('Hola Mundo¡¡¡lll')
